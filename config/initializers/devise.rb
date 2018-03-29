a1451e33776be07cef1e9d40c7314367a783597e
vladfreel <vladfreelmaster@gmail.com>
2018-03-29 15:00:54 +0300

    Add username to user

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -33,6 +33,8 @@ Devise.setup do |config|
   require 'devise/orm/active_record'
 
   # ==> Configuration for any authentication mechanism
+
+  config.authentication_keys = [ :login ]
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
   # authenticating a user, both parameters are required. Remember that those
