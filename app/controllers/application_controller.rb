68a53872506567373463b6f71aa03887b86d4b23
vladfreel <vladfreelmaster@gmail.com>
2018-04-25 10:00:38 +0300

    Add pundit with all specifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   protected
 
@@ -9,5 +11,12 @@ class ApplicationController < ActionController::Base
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me, :avatar]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:warning] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
   end
 end
--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,14 +1,17 @@
 class ChatRoomsController < ApplicationController
   def index
     @chat_rooms = ChatRoom.all
+    authorize @chat_rooms
   end
 
   def new
     @chat_room = ChatRoom.new
+    authorize @chat_room
   end
 
   def create
     @chat_room = current_user.chat_rooms.build(chat_room_params)
+    authorize @chat_room
     if @chat_room.save
       flash[:success] = 'Chat room added!'
       redirect_to @chat_room
@@ -19,6 +22,7 @@ class ChatRoomsController < ApplicationController
 
   def show
     @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+    authorize @chat_room
     @message = Message.new
   end
 
