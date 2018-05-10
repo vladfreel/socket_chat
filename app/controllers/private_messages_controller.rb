d7cd1e903c688af6da585b3995dc1e9c3d624a70
vladfreel <vladfreelmaster@gmail.com>
2018-05-10 14:20:51 +0300

    Add some changes

--- app/controllers/private_messages_controller.rb
+++ app/controllers/private_messages_controller.rb
@@ -1,2 +1,18 @@
 class PrivateMessagesController < ApplicationController
+  def update
+    message = PrivateMessage.find(params[:id])
+    message.update!(private_message_params)
+    redirect_to private_chat_room_path(message.private_chat_room.id)
+  end
+
+  def destroy
+    message = PrivateMessage.find(params[:id])
+    message.destroy
+    redirect_to private_chat_room_path(message.private_chat_room.id)
+  end
+  private
+
+  def private_message_params
+    params.require(:private_message).permit(:content)
+  end
 end
