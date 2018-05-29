ee7777dccd64419587e0833645521f834624c659
Anton <capitalsav@gmail.com>
2018-05-29 11:35:15 +0300

    Update routes - remove resources users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :user_workspaces
   resources :workspaces
   devise_for :users, controllers: { confirmations: 'confirmations' }
-  resources :users
+
   resources :chat_rooms, only: [:new, :create, :show, :index]
   resources :private_chat_rooms, except: [:update, :edit, :destroy]
   resources :multi_user_private_chat_rooms
